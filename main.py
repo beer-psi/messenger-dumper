41ef23e3d57c3bf56f5645210b307427c44c9bb1
beerpiss <lacvtg.a1.2023@gmail.com>
2023-09-20 16:38:33 +0700

    do not the markdown

--- main.py
+++ main.py
@@ -8,12 +8,67 @@ import getpass
 import mimetypes
 import os
 import time
+import re
 from typing import Any
 
 from maufbapi import AndroidAPI, AndroidState
 from maufbapi.http.errors import RateLimitExceeded
 from maufbapi.types.graphql import Message, MinimalSticker, Attachment, AttachmentType
 from mautrix.util.proxy import ProxyHandler
+
+
+_MARKDOWN_ESCAPE_SUBREGEX = '|'.join(r'\{0}(?=([\s\S]*((?<!\{0})\{0})))'.format(c) for c in ('*', '`', '_', '~', '|'))
+
+_MARKDOWN_ESCAPE_COMMON = r'^>(?:>>)?\s|\[.+\]\(.+\)|^#{1,3}|^\s*-'
+
+_MARKDOWN_ESCAPE_REGEX = re.compile(fr'(?P<markdown>{_MARKDOWN_ESCAPE_SUBREGEX}|{_MARKDOWN_ESCAPE_COMMON})', re.MULTILINE)
+
+_URL_REGEX = r'(?P<url><[^: >]+:\/[^ >]+>|(?:https?|steam):\/\/[^\s<]+[^<.,:;\"\'\]\s])'
+
+_MARKDOWN_STOCK_REGEX = fr'(?P<markdown>[_\\~|\*`]|{_MARKDOWN_ESCAPE_COMMON})'
+
+
+def escape_markdown(text: str, *, as_needed: bool = False, ignore_links: bool = True) -> str:
+    r"""A helper function that escapes Discord's markdown.
+
+    Parameters
+    -----------
+    text: :class:`str`
+        The text to escape markdown from.
+    as_needed: :class:`bool`
+        Whether to escape the markdown characters as needed. This
+        means that it does not escape extraneous characters if it's
+        not necessary, e.g. ``**hello**`` is escaped into ``\*\*hello**``
+        instead of ``\*\*hello\*\*``. Note however that this can open
+        you up to some clever syntax abuse. Defaults to ``False``.
+    ignore_links: :class:`bool`
+        Whether to leave links alone when escaping markdown. For example,
+        if a URL in the text contains characters such as ``_`` then it will
+        be left alone. This option is not supported with ``as_needed``.
+        Defaults to ``True``.
+
+    Returns
+    --------
+    :class:`str`
+        The text with the markdown special characters escaped with a slash.
+    """
+
+    if not as_needed:
+
+        def replacement(match):
+            groupdict = match.groupdict()
+            is_url = groupdict.get('url')
+            if is_url:
+                return is_url
+            return '\\' + groupdict['markdown']
+
+        regex = _MARKDOWN_STOCK_REGEX
+        if ignore_links:
+            regex = f'(?:{_URL_REGEX}|{regex})'
+        return re.sub(regex, replacement, text, 0, re.MULTILINE)
+    else:
+        text = re.sub(r'\\', r'\\\\', text)
+        return _MARKDOWN_ESCAPE_REGEX.sub(r'\\\1', text)
 
 
 def generate_state() -> AndroidState:
@@ -207,7 +262,7 @@ async def convert_message(
                 if content:
                     converted["a"].append(content)
     if message.message:
-        converted["m"] = message.message.text
+        converted["m"] = escape_markdown(message.message.text)
     if message.replied_to_message and message.replied_to_message.message.message_id:
         replied_numeric_id = message.replied_to_message.message.message_id.split(".")[1]
         converted["r"] = replied_numeric_id
